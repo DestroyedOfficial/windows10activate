3c5a28f818d750de121381be38c3799cab08c409
DestroyedOfficial <destroyedofficial15@gmail.com>
2024-12-25 18:56:27 +0100

    Add files via upload

---
+++ Win10_Activate_by_Destroyed.bat
@@ -0,0 +1,17 @@
+@echo off
+title Windows 10 Pro Aktivalo By Destroyed
+cls
+echo.
+echo.
+echo.
+echo      Varj egy kicsit es aktivalva lesz a Windows, amit majd a nemsoka felugro ablakokban is latni fogsz.
+echo      Nyomj meg barmilyen gombot, hogy elkezdd az aktivalast
+echo.
+echo.
+pause >nul
+echo.
+echo.
+echo.
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
